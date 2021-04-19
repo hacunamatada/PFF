3fabc5f6eddce3ad8f84361025d2589664ba7b88
cafemesa <c.f.medina@hotmail.com>
2021-04-19 09:58:09 +0200

    Update

--- src/kinect_pff.cpp
+++ src/kinect_pff.cpp
@@ -6,144 +6,169 @@
 #include <sensor_msgs/LaserScan.h>
 #define PI 3.14159265
 
- 
-ros::Publisher scan_pub,laserCloud_pub;
-float robotheight, sensorheight, resolution;
+class pff
+{
+public:
+	pff();
+	void filter(const sensor_msgs::PointCloud2ConstPtr &input);
+	float robotheight, sensorheight, resolution;
+	int visionangle, position;
+	std::string topic_pub, topic_sub, frame_id;
+	struct Point3D
+	{
 
-//Struct to access for each point cordenates
-struct Point3D {
+		float x;
+		float y;
+		float z;
+		Point3D(float x_, float y_, float z_)
+			: x(x_), y(y_), z(z_)
+		{
+		}
+	};
 
-  float x;
-  float y;
-  float z;
-  Point3D(float x_, float y_, float z_)
-    : x(x_), y(y_), z(z_) {
-
-  }
+private:
+	ros::NodeHandle node_, ns_;
+	ros::Publisher scan_pub;
+	ros::Subscriber sub;
 };
 
-/**
- * This function recieve the Velodyne PointCloud. 
- * Dicard points with a vertical filter
- * Discar points with a horizontal filter
- * Detect stairs with the points with height less than the floor height
- * Project the points into 2D plane
- * Extract the nearest point for each angle
- * Convert to LaserScan
- * Publish
- *
- * @param input Astra Camera PointCloud directly from the sensor
- */
+pff::pff() : node_("~"),
+			 robotheight(1.0),
+			 sensorheight(0.57),
+			 visionangle(360),
+			 resolution(0.2),
+			 topic_pub("/scan"),
+			 topic_sub("/velodyne_points"),
+			 frame_id("base_scan")
+{
+	/** Get parameters from the launch file */
+	node_.param("robot_height", robotheight, robotheight);
+	node_.param("sensor_height", sensorheight, sensorheight);
+	node_.param("horizontal_fov", visionangle, visionangle);
+	node_.param("resolution", resolution, resolution);
+	node_.param("topic_pub", topic_pub, topic_pub);
+	node_.param("topic_sub", topic_sub, topic_sub);
+	node_.param("frame_id", frame_id, frame_id);
 
-void 
-cloud_cb2 (const sensor_msgs::PointCloud2ConstPtr& input) 
+	/** Define Subscriber */
+	sub = ns_.subscribe(topic_sub, 50, &pff::filter, this);
+
+	/** Define Publisher */
+	scan_pub = ns_.advertise<sensor_msgs::LaserScan>(topic_pub, 1, false);
+}
+
+void pff::filter(const sensor_msgs::PointCloud2ConstPtr &input)
 {
 
-  int numPts = input->height * input->width;
-  char* raw3DPtsData = (char*)(input->data.data());
-  std::vector<Point3D> pc(numPts, Point3D(0.,0.,0.));
+	int numPts = input->height * input->width;
+	char* raw3DPtsData = (char*)(input->data.data());
+	std::vector<Point3D> pc(numPts, Point3D(0.,0.,0.));
 
-  pcl::PointCloud<pcl::PointXYZ> laserCloudIn;
-  pcl::PointCloud<pcl::PointXYZ> laserCloudOut;
-  laserCloudOut.points.resize(numPts);
-  pcl::fromROSMsg(*input, laserCloudIn);
-  pcl::PointXYZI point;  
+	pcl::PointCloud<pcl::PointXYZ> laserCloudIn;
+	pcl::fromROSMsg(*input, laserCloudIn);
 
-  //Laser Transform variables
-	unsigned int num_readings = 360/resolution;
-  double laser_frequency = 600;
-  double ranges[num_readings]={0};
-  double intensities[num_readings]={0};
-	int addpos=0;
-	int position=0;
-    
-    
-	for (int i = 0; i < laserCloudIn.size(); i++) {   
-           
-    float* base = (float*)(raw3DPtsData + i * input->point_step);
-    Point3D point3d(0.,0.,0.);       
+	/** Set scan parameters */
+	ros::Time scan_time = ros::Time::now();
+	sensor_msgs::LaserScan scan;
+	scan.header.stamp = scan_time;
+	scan.header.frame_id = frame_id;
+	scan.angle_min = PI;
+	scan.angle_max = -PI;
+	scan.angle_increment = -2 * PI / (360 / resolution);
+	scan.time_increment = 1 / (600 * (360 / resolution));
+	scan.range_min = 0.2;
+	scan.range_max = 4.0;
+	scan.ranges.resize(360 / resolution);
+	scan.intensities.resize(360 / resolution);
 
-    point.x=base[2];
-    point.y=-base[0];
-    point.z=-base[1];
+	for (int i = 0; i < laserCloudIn.size(); i++)
+	{
+		float* base = (float*)(raw3DPtsData + i * input->point_step);
+		Point3D point3d(0.,0.,0.);       
 
-    // Vertical filter : Points with a height greater than the height of the robot are discarded
-    if (point.z < -sensorheight || point.z > robotheight-sensorheight || isnan(point.x)==1 || isnan(point.y)==1) {continue;}
+		laserCloudIn[i].x=base[2];
+		laserCloudIn[i].y=-base[0];
+		laserCloudIn[i].z=-base[1];
 
-    float hip = sqrt((point.x)*(point.x)+((point.y)*(point.y)));
-		float hangle = (asin ((point.x)/hip))*180/PI;     
+		float hip = sqrt(pow(laserCloudIn[i].x, 2) + pow(laserCloudIn[i].y, 2));
+		float hangle = (asin((laserCloudIn[i].x) / hip)) * 180 / PI;
 
-    //Fliter nearest point per angle resolution
-    if (point.y>0) {
-      position=(180.0+hangle)/resolution;
-    }
-    else if (point.y<=0) {
-      position=(360.0-hangle)/resolution;
-    }
+		/** Discard points */
+		if (laserCloudIn[i].z > robotheight - sensorheight ||
+			(-sensorheight - 0.1 <= laserCloudIn[i].z && laserCloudIn[i].z < -sensorheight + 0.05) ||
+			(visionangle <= 180 && laserCloudIn[i].x <= 0) ||
+			(visionangle < 180 && laserCloudIn[i].x > 0 && hangle < 90 - (visionangle / 2)) ||
+			(visionangle > 180 && visionangle < 360 && laserCloudIn[i].x < 0 && abs(hangle) > (visionangle - 180) / 2) ||
+			(isnan(laserCloudIn[i].x)==1 || isnan(laserCloudIn[i].y)==1) || hip>4.0)
+		{
+			continue;
+		}
 
-    if (ranges[position]==0 || hip < ranges[position])
-    {
-      ranges[position]=hip;
-      intensities[position]=50;
-    }	
+		/** Stairs/holes detection */
+		if (laserCloudIn[i].z < -sensorheight - 0.1)
+		{
+			float hip2 = sqrt(pow(hip, 2) + pow(laserCloudIn[i].z, 2));
+			float hangle2 = asin((laserCloudIn[i].z / hip2) * sin(1.5708)) * 180 / PI;
+			if (hangle2 < -14)
+			{
+				hip = 0.22;
+				laserCloudIn[i].x = hip * sin(hangle * PI / 180);
+				if (laserCloudIn[i].y > 0)
+				{
+					laserCloudIn[i].y = sqrt(pow(hip, 2) - pow(laserCloudIn[i].x, 2));
+				}
+				else
+				{
+					laserCloudIn[i].y = -sqrt(pow(hip, 2) - pow(laserCloudIn[i].x, 2));
+				}
+			}
+			if (hangle2 > -14)
+			{
+				hip2 = sensorheight * sin(PI / 2) / sin((2 - hangle2) * PI / 180);
+				hip = sqrt(pow(hip2, 2) - pow(sensorheight, 2));
+				laserCloudIn[i].x = hip * sin(hangle * PI / 180);
+				if (laserCloudIn[i].y > 0)
+				{
+					laserCloudIn[i].y = sqrt(pow(hip, 2) - pow(laserCloudIn[i].x, 2));
+				}
+				else
+				{
+					laserCloudIn[i].y = -sqrt(pow(hip, 2) - pow(laserCloudIn[i].x, 2));
+				}
+			}
+		}
 
-    laserCloudOut.points[i].x=point.x;
-		laserCloudOut.points[i].y=point.y;
-		laserCloudOut.points[i].z=point.z;	     
+		/** Get the position in the array */
+		if (laserCloudIn[i].y > 0)
+		{
+			position = (180.0 + hangle) / resolution;
+		}
+		else if (laserCloudIn[i].x > 0 && laserCloudIn[i].y <= 0)
+		{
+			position = (360.0 - hangle) / resolution;
+		}
+		else
+		{
+			position = -hangle / resolution;
+		}
 
-  }
-  //Publish LegsCloud
-	sensor_msgs::PointCloud2 laserCloudOut_output;
-	pcl::toROSMsg(laserCloudOut, laserCloudOut_output);
-  laserCloudOut_output.header.frame_id = "base_link";
-	laserCloud_pub.publish (laserCloudOut_output);
+		/** Add near point to the scan array */
+		if (scan.ranges[position] == 0 || hip < scan.ranges[position])
+		{
+			scan.ranges[position] = hip;
+			scan.intensities[position] = 100;
+		}
+	}
 
-  // Configuration of parameters for conversion to LaserScan
-  ros::Time scan_time = ros::Time::now();
-  sensor_msgs::LaserScan scan;
-  scan.header.stamp = scan_time;
-  scan.header.frame_id = "base_link";
-  scan.angle_min = 3*PI/2;
-  scan.angle_max = -PI/2;
-  scan.angle_increment = -6.28 / num_readings;
-  scan.time_increment = (1 /  num_readings);
-  scan.range_min = 0.2;
-  scan.range_max = 10.0;
-  scan.ranges.resize(num_readings);
-  scan.intensities.resize(num_readings);
-
-  // assign values to the vector of ranges and intensities to publish the LaserScan
-  for(unsigned int i = 0; i < num_readings; ++i){
-    scan.ranges[i] = ranges[i];
-    scan.intensities[i] = intensities[i];
-  }  
-
-  // Publishing of the LaserScan topic
-  scan_pub.publish(scan);	
-
-    
+	/** Publish Scan */
+	scan_pub.publish(scan);
 }
 
-int 
-main (int argc, char **argv)
-{    
-    // Initialize ROS
-    ros::init (argc, argv, "kinect_pff");
-    ros::NodeHandle nh;
-
-    // Get launch parameters
-    if(!nh.getParam("/kinect_pff/robot_height",robotheight)){robotheight = 0.6;}
-    if(!nh.getParam("/kinect_pff/sensor_height",sensorheight)){sensorheight = 0.3;}
-    if(!nh.getParam("/kinect_pff/resolution",resolution)){resolution = 0.2;}
-    
-    // Create a ROS subscriber for the input PointCloud
-    ros::Subscriber sub = nh.subscribe ("/camera/depth/points", 1, cloud_cb2);
-    
-    // Create a ROS publisher for the output LaserScan
-    scan_pub = nh.advertise<sensor_msgs::LaserScan>("/scan", 1);
-    laserCloud_pub = nh.advertise<sensor_msgs::PointCloud2> ("/PeopleCloud", 1);
-    
-    // Spin
-    ros::spin ();
+/** Main function*/
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "astra_pff");
+	pff filter;
+	ros::spin();
+	return 0;
 }
-
--- src/velodyne.cpp
+++ src/velodyne.cpp
@@ -8,38 +8,28 @@
 
 class pff
 {
-public:
-	pff();
-	void filter(const sensor_msgs::PointCloud2ConstPtr &input);
-	float robotheight, sensorheight, resolution;
-	int visionangle, position;
-	std::string topic_pub, topic_sub, frame_id;
-	struct Point3D
-	{
+	public:
+		pff();
+		void filter(const sensor_msgs::PointCloud2ConstPtr &input);	
+		float robotheight, sensorheight, resolution;
+		int visionangle, position;
+		std::string topic_pub, topic_sub, frame_id;			
 
-		float x;
-		float y;
-		float z;
-		Point3D(float x_, float y_, float z_)
-			: x(x_), y(y_), z(z_)
-		{
-		}
-	};
-
-private:
-	ros::NodeHandle node_, ns_;
-	ros::Publisher scan_pub;
-	ros::Subscriber sub;
+	private:
+		ros::NodeHandle node_, ns_;
+		ros::Publisher scan_pub;
+		ros::Subscriber sub;
 };
 
-pff::pff() : node_("~"),
-			 robotheight(1.0),
-			 sensorheight(0.57),
-			 visionangle(360),
-			 resolution(0.2),
-			 topic_pub("/scan"),
-			 topic_sub("/velodyne_points"),
-			 frame_id("base_scan")
+pff::pff():
+node_("~"),
+robotheight(1.0),
+sensorheight(0.57),
+visionangle(360),
+resolution(0.2),
+topic_pub("/scan"),
+topic_sub("/livox/lidar"),
+frame_id("base_link")
 {
 	/** Get parameters from the launch file */
 	node_.param("robot_height", robotheight, robotheight);
@@ -49,21 +39,16 @@ pff::pff() : node_("~"),
 	node_.param("topic_pub", topic_pub, topic_pub);
 	node_.param("topic_sub", topic_sub, topic_sub);
 	node_.param("frame_id", frame_id, frame_id);
-
+	
 	/** Define Subscriber */
 	sub = ns_.subscribe(topic_sub, 50, &pff::filter, this);
 
 	/** Define Publisher */
-	scan_pub = ns_.advertise<sensor_msgs::LaserScan>(topic_pub, 1, false);
+	scan_pub = ns_.advertise<sensor_msgs::LaserScan>(topic_pub, 1,false);
 }
 
 void pff::filter(const sensor_msgs::PointCloud2ConstPtr &input)
 {
-
-	int numPts = input->height * input->width;
-	char* raw3DPtsData = (char*)(input->data.data());
-	std::vector<Point3D> pc(numPts, Point3D(0.,0.,0.));
-
 	pcl::PointCloud<pcl::PointXYZ> laserCloudIn;
 	pcl::fromROSMsg(*input, laserCloudIn);
 
@@ -72,34 +57,27 @@ void pff::filter(const sensor_msgs::PointCloud2ConstPtr &input)
 	sensor_msgs::LaserScan scan;
 	scan.header.stamp = scan_time;
 	scan.header.frame_id = frame_id;
-	scan.angle_min = PI;
-	scan.angle_max = -PI;
+	scan.angle_min = 3 * PI / 2;
+	scan.angle_max = -PI / 2;
 	scan.angle_increment = -2 * PI / (360 / resolution);
 	scan.time_increment = 1 / (600 * (360 / resolution));
-	scan.range_min = 0.2;
-	scan.range_max = 4.0;
+	scan.range_min = 0.1;
+	scan.range_max = 50.0;
 	scan.ranges.resize(360 / resolution);
 	scan.intensities.resize(360 / resolution);
 
 	for (int i = 0; i < laserCloudIn.size(); i++)
 	{
-		float* base = (float*)(raw3DPtsData + i * input->point_step);
-		Point3D point3d(0.,0.,0.);       
 
-		laserCloudIn[i].x=base[0];
-		laserCloudIn[i].y=base[2];
-		laserCloudIn[i].z=-base[1];
-
-		float hip = sqrt(pow(laserCloudIn[i].x, 2) + pow(laserCloudIn[i].y, 2));
+		float hip = sqrt(pow(laserCloudIn[i].x,2) + pow(laserCloudIn[i].y,2));
 		float hangle = (asin((laserCloudIn[i].x) / hip)) * 180 / PI;
 
 		/** Discard points */
-		if (laserCloudIn[i].z > robotheight - sensorheight ||
-			(-sensorheight - 0.1 <= laserCloudIn[i].z && laserCloudIn[i].z < -sensorheight + 0.05) ||
+		if 	(laserCloudIn[i].z > robotheight - sensorheight || 
+			(/*-sensorheight - 0.1 <= laserCloudIn[i].z &&*/ laserCloudIn[i].z < -sensorheight + 0.15) ||
 			(visionangle <= 180 && laserCloudIn[i].x <= 0) ||
 			(visionangle < 180 && laserCloudIn[i].x > 0 && hangle < 90 - (visionangle / 2)) ||
-			(visionangle > 180 && visionangle < 360 && laserCloudIn[i].x < 0 && abs(hangle) > (visionangle - 180) / 2) ||
-			(isnan(laserCloudIn[i].x)==1 || isnan(laserCloudIn[i].y)==1) || hip>4.0)
+			(visionangle > 180 && visionangle < 360 && laserCloudIn[i].x < 0 && abs(hangle) > (visionangle - 180) / 2))
 		{
 			continue;
 		}
@@ -165,10 +143,10 @@ void pff::filter(const sensor_msgs::PointCloud2ConstPtr &input)
 }
 
 /** Main function*/
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "astra_pff");
-	pff filter;
-	ros::spin();
-	return 0;
+    ros::init(argc, argv, "velodyne_pff");
+    pff filter;
+    ros::spin();
+    return 0;
 }
